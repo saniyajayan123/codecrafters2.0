cb384c56f1fc59c65b1bf8570629d249c2642f88
saniyajayan123 <yasani709@gmail.com>
2024-10-09 10:53:18 +0000

    condition

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 
+number_1: int=(input("Enter a number 1:"))
+number_2: int=(input("Enter a number 2:"))
+if number_1 == number_2:
+    print("Both are same")
+else:
+     print("both are different")
+   number_1:int=float("enter a number 1:")    
+
